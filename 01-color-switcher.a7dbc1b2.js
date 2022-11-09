1bf4725d6f2c1b18f70b567afc553da9b5aada75
alaskaart <112183113+alaskaart@users.noreply.github.com>
2022-11-09 07:10:36 +0000

    Deploying to gh-pages from @ alaskaart/goit-js-hw-09@75d5104d1d9868292df6f3dfc21c66b367581ce6 🚀

---
+++ 01-color-switcher.a7dbc1b2.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.a7dbc1b2.js.map
---
+++ 01-color-switcher.ea37d495.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let n=null;t.addEventListener("click",(function(){n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.ea37d495.js.map
